0fef6dcc6d125da972b17ef85676b36ab1dc2651
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-01-18 18:16:30 +0100

    Add unary_expression

--- src/main.rs
+++ src/main.rs
@@ -106,6 +106,39 @@ fn transform_value(value: &lua_ast::Value) -> Expr {
     }
 }
 
+fn transform_unary_expression(op: &lua_ast::UnOp, expression: &lua_ast::Expression) -> Expr {
+    if matches!(op, lua_ast::UnOp::Hash(_)) {
+        Expr::Call(CallExpr {
+            span: Default::default(),
+            args: vec![],
+            type_args: None,
+            callee: Callee::Expr(boxed(Expr::Member(MemberExpr {
+                span: Default::default(),
+                obj: boxed(transform_expression(expression)),
+                prop: MemberProp::Ident(Ident {
+                    span: Default::default(),
+                    sym: JsWord::from("size"),
+                    optional: false,
+                }),
+            }))),
+        })
+    } else {
+        match op {
+            lua_ast::UnOp::Not(_) => Expr::Unary(UnaryExpr {
+                span: Default::default(),
+                op: UnaryOp::Bang,
+                arg: boxed(transform_expression(expression)),
+            }),
+            lua_ast::UnOp::Minus(_) => Expr::Unary(UnaryExpr {
+                span: Default::default(),
+                op: UnaryOp::Minus,
+                arg: boxed(transform_expression(expression)),
+            }),
+            _ => skip("Unknown unary operator", op),
+        }
+    }
+}
+
 fn transform_expression(expr: &lua_ast::Expression) -> Expr {
     match expr {
         lua_ast::Expression::Parentheses {
@@ -115,6 +148,9 @@ fn transform_expression(expr: &lua_ast::Expression) -> Expr {
             span: Default::default(),
             expr: boxed(transform_expression(expression)),
         }),
+        lua_ast::Expression::UnaryOperator { unop, expression } => {
+            transform_unary_expression(unop, expression)
+        }
         lua_ast::Expression::Value {
             value,
             type_assertion,
