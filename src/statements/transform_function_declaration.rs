044119c97e15d9006635b1c1967a82833dfadcd2
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-01-20 11:00:11 +0100

    Separate function_declaration

---
+++ src/statements/transform_function_declaration.rs
@@ -0,0 +1,71 @@
+use crate::prelude::*;
+
+fn transform_function_name(name: &lua_ast::FunctionName) -> PatOrExpr {
+	let mut iter = name.names().iter();
+	let mut result = Expr::Ident(Ident {
+		span: Default::default(),
+		sym: JsWord::from(iter.next().unwrap().token().to_string()),
+		optional: false,
+	});
+	for token in iter {
+		result = Expr::Member(MemberExpr {
+			span: Default::default(),
+			obj: boxed(result),
+			prop: MemberProp::Ident(Ident {
+				span: Default::default(),
+				sym: JsWord::from(token.token().to_string()),
+				optional: false,
+			}),
+		});
+	}
+	if let Some(token) = name.method_name() {
+		result = Expr::Member(MemberExpr {
+			span: Default::default(),
+			obj: boxed(result),
+			prop: MemberProp::Ident(Ident {
+				span: Default::default(),
+				sym: JsWord::from(token.token().to_string()),
+				optional: false,
+			}),
+		});
+	}
+	if let Expr::Ident(ident) = result {
+		PatOrExpr::Pat(boxed(Pat::Ident(BindingIdent::from(ident))))
+	} else {
+		PatOrExpr::Expr(boxed(result))
+	}
+}
+
+pub fn transform_function_declaration(declaration: &lua_ast::FunctionDeclaration) -> Stmt {
+	Stmt::Expr(ExprStmt {
+		span: Default::default(),
+		expr: boxed(Expr::Assign(AssignExpr {
+			span: Default::default(),
+			left: transform_function_name(declaration.name()),
+			op: AssignOp::Assign,
+			right: boxed(Expr::Fn(FnExpr {
+				ident: None,
+				function: Function {
+					span: Default::default(),
+					is_async: false,
+					is_generator: false,
+					return_type: None,
+					type_params: None,
+					decorators: vec![],
+					params: transform_function_params(declaration.body().parameters().iter())
+						.into_iter()
+						.map(|param| Param {
+							span: Default::default(),
+							decorators: vec![],
+							pat: param,
+						})
+						.collect(),
+					body: Some(BlockStmt {
+						span: Default::default(),
+						stmts: transform_block_statements(declaration.body().block()),
+					}),
+				},
+			})),
+		})),
+	})
+}
--- src/statements/transform_statement.rs
+++ src/statements/transform_statement.rs
@@ -8,41 +8,9 @@ pub fn transform_statement(stmt: &lua_ast::Stmt) -> Stmt {
 			span: Default::default(),
 			expr: boxed(transform_function_call(function_call)),
 		}),
-		lua_ast::Stmt::FunctionDeclaration(declaration) => Stmt::Expr(ExprStmt {
-			span: Default::default(),
-			expr: boxed(Expr::Assign(AssignExpr {
-				span: Default::default(),
-				left: PatOrExpr::Pat(boxed(Pat::Ident(BindingIdent::from(Ident::new(
-					// TODO: Fix this for method calls
-					JsWord::from(declaration.name().to_string()),
-					Default::default(),
-				))))),
-				op: AssignOp::Assign,
-				right: boxed(Expr::Fn(FnExpr {
-					ident: None,
-					function: Function {
-						span: Default::default(),
-						is_async: false,
-						is_generator: false,
-						return_type: None,
-						type_params: None,
-						decorators: vec![],
-						params: transform_function_params(declaration.body().parameters().iter())
-							.into_iter()
-							.map(|param| Param {
-								span: Default::default(),
-								decorators: vec![],
-								pat: param,
-							})
-							.collect(),
-						body: Some(BlockStmt {
-							span: Default::default(),
-							stmts: transform_block_statements(declaration.body().block()),
-						}),
-					},
-				})),
-			})),
-		}),
+		lua_ast::Stmt::FunctionDeclaration(declaration) => {
+			transform_function_declaration(declaration)
+		}
 		lua_ast::Stmt::GenericFor(generic_for) => {
 			skip_stmt("generic for loops not yet implemented", generic_for)
 		}
