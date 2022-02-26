f0f2a0d1aabc8a83c715da3d57949a01e2587806
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-02-26 19:12:18 +0100

    Better transform_string

--- src/expressions/transform_string.rs
+++ src/expressions/transform_string.rs
@@ -1,15 +1,15 @@
 use crate::prelude::*;
 
-pub fn transform_string(number: &tokenizer::TokenReference) -> Expr {
-	let content = if let tokenizer::TokenType::StringLiteral {
-		literal,
-		multi_line: _,
-		quote_type: _,
-	} = number.token().token_type()
-	{
-		literal.as_str()
-	} else {
-		unreachable!("Must be tring literal");
-	};
-	Expr::Lit(Lit::Str(make_string(content)))
+pub fn transform_string(token: &tokenizer::TokenReference) -> Expr {
+	match token.token().token_type() {
+		tokenizer::TokenType::StringLiteral {
+			literal,
+			multi_line: _,
+			quote_type: _,
+		} => Expr::Lit(Lit::Str(make_string(literal))),
+		_ => skip(
+			"transform_string token was not TokenType::StringLiteral",
+			token,
+		),
+	}
 }
