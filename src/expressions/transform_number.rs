870bb2a960995cdd25c8d4f21e13212b8e622d67
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-02-26 19:15:58 +0100

    Better number parsing

--- src/expressions/transform_number.rs
+++ src/expressions/transform_number.rs
@@ -1,15 +1,28 @@
 use crate::prelude::*;
 
-pub fn transform_number(number: &tokenizer::TokenReference) -> Expr {
+// Taken and adapted from StyLua under the MPL-2.0 license
+// https://github.com/JohnnyMorganz/StyLua/blob/7efc7cbd91a4b8235ea4e8c6b07b7028fc1534b7/src/verify_ast.rs#L129-L147
+pub fn transform_number(token: &tokenizer::TokenReference) -> Expr {
+	let text = match token.token_type() {
+		tokenizer::TokenType::Number { text } => text,
+		_ => return skip("transform_number token was not TokenType::Number", token),
+	};
+	let text = text.replace('_', "");
+	let number = match text.as_str().parse::<f64>() {
+		Ok(num) => num,
+		// Try parsing as Hex (0x)
+		Err(_) => match i32::from_str_radix(&text[2..], 16) {
+			Ok(num) => num.into(),
+			// Try parsing as binary (0b)
+			Err(_) => match i32::from_str_radix(&text.as_str()[2..], 2) {
+				Ok(num) => num.into(),
+				// Will have been full_moon tokenizer error
+				Err(_) => unreachable!(),
+			},
+		},
+	};
 	Expr::Lit(Lit::Num(Number {
 		span: Default::default(),
-		value: number
-			.token()
-			.to_string()
-			.chars()
-			.filter(|c| c != &'_')
-			.collect::<String>()
-			.parse()
-			.unwrap(),
+		value: number,
 	}))
 }
