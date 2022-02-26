e105d9e1e24c41a52ba34cf9ec7685551553ef77
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-02-26 03:14:33 +0100

    Add experimental handling of computed number keys

--- src/expressions/transform_table_constructor.rs
+++ src/expressions/transform_table_constructor.rs
@@ -46,10 +46,19 @@ pub fn transform_table_constructor(args: &lua_ast::TableConstructor) -> Expr {
 		.fields()
 		.iter()
 		.all(|field| matches!(field, lua_ast::Field::NoKey(_)));
-	let is_object = args
-		.fields()
-		.iter()
-		.all(|field| matches!(field, lua_ast::Field::NameKey { .. }));
+	let is_object = args.fields().iter().all(|field| {
+		matches!(field, lua_ast::Field::NameKey { .. })
+			|| matches!(
+				field,
+				lua_ast::Field::ExpressionKey {
+					key: lua_ast::Expression::Value {
+						value,
+						type_assertion: _,
+					},
+					..
+				} if matches!(**value, lua_ast::Value::Number(_))
+			)
+	});
 	if is_object {
 		Expr::Object(ObjectLit {
 			span: Default::default(),
@@ -57,19 +66,49 @@ pub fn transform_table_constructor(args: &lua_ast::TableConstructor) -> Expr {
 				.fields()
 				.iter()
 				.map(|field| {
-					if let lua_ast::Field::NameKey {
-						key,
-						value,
-						equal: _,
-					} = field
-					{
-						PropOrSpread::Prop(boxed(Prop::KeyValue(KeyValueProp {
-							key: PropName::Str(make_string(&key.token().to_string())),
-							value: boxed(transform_expression(value)),
-						})))
-					} else {
-						unreachable!("Checked to be all NameKey variant earlier")
-					}
+					PropOrSpread::Prop(boxed(Prop::KeyValue(
+						if let lua_ast::Field::NameKey {
+							key,
+							value,
+							equal: _,
+						} = field
+						{
+							KeyValueProp {
+								key: PropName::Str(make_string(&key.token().to_string())),
+								value: boxed(transform_expression(value)),
+							}
+						} else if let lua_ast::Field::ExpressionKey {
+							key:
+								lua_ast::Expression::Value {
+									value: key,
+									type_assertion: _,
+								},
+							value,
+							equal: _,
+							brackets: _,
+						} = field
+						{
+							let key = if let lua_ast::Value::Number(ref key) = **key {
+								if let tokenizer::TokenType::Number { text } = key.token_type() {
+									text
+								} else {
+									unreachable!("Value::Number only has TokenType::Number")
+								}
+							} else {
+								unreachable!("Checked to be Value::Number earlier")
+							};
+							KeyValueProp {
+								key: PropName::Num(Number::from(
+									// TODO: See if full_moon provides this without manual parsing
+									// BUG: parse will panic on lua syntax like `1_000` or `0b0000`
+									key.to_string().parse::<f64>().unwrap(),
+								)),
+								value: boxed(transform_expression(value)),
+							}
+						} else {
+							unreachable!("Checked to be Field::NameKey earlier")
+						},
+					)))
 				})
 				.collect(),
 		})
