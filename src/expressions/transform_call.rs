ca4472889994d7f6f268bec3a56604641d8793cf
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-04-11 14:52:46 +0200

    Rework call transform to support .new()

--- src/expressions/transform_call.rs
+++ src/expressions/transform_call.rs
@@ -1,27 +1,50 @@
 use crate::prelude::*;
 
 pub fn transform_call(call: &lua_ast::Call, base: Expr) -> Expr {
-	let mut result = base;
-	let args = if let lua_ast::Call::MethodCall(method_call) = call {
-		result = Expr::Member(MemberExpr {
-			span: Default::default(),
-			obj: boxed(result),
-			prop: MemberProp::Ident(Ident {
+	let (result, args) = match call {
+		lua_ast::Call::MethodCall(method_call) => (
+			Expr::Member(MemberExpr {
 				span: Default::default(),
-				optional: false,
-				sym: JsWord::from(method_call.name().token().to_string()),
+				obj: boxed(base),
+				prop: MemberProp::Ident(Ident {
+					span: Default::default(),
+					optional: false,
+					sym: JsWord::from(method_call.name().token().to_string()),
+				}),
 			}),
-		});
-		method_call.args()
-	} else if let lua_ast::Call::AnonymousCall(args) = call {
-		args
-	} else {
-		return skip("Unknown call type", call);
+			transform_function_args(method_call.args()),
+		),
+		lua_ast::Call::AnonymousCall(args) => {
+			if let Expr::Member(MemberExpr {
+				prop: MemberProp::Ident(Ident {
+					sym: js_word!("new"),
+					..
+				}),
+				obj,
+				span: _,
+			}) = base
+			{
+				return Expr::New(NewExpr {
+					span: Default::default(),
+					callee: obj,
+					args: Some(transform_function_args(args)),
+					type_args: None,
+				});
+			}
+			(base, transform_function_args(args))
+		}
+		_ => (
+			base,
+			vec![ExprOrSpread {
+				spread: None,
+				expr: boxed(skip("Unknown call type", call)),
+			}],
+		),
 	};
 	Expr::Call(CallExpr {
 		span: Default::default(),
 		type_args: None,
 		callee: Callee::Expr(boxed(result)),
-		args: transform_function_args(args),
+		args,
 	})
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 pub use full_moon::{ast as lua_ast, node, tokenizer};
 
-pub use swc_atoms::JsWord;
+pub use swc_atoms::{js_word, JsWord};
 pub use swc_ecma_ast::*;
 
 pub use crate::expressions::*;
