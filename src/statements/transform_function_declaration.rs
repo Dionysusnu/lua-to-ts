f5b2ddc4c71f018225fa6c8cbf3598beb6ce0049
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-07-21 17:43:35 +0200

    Use direct function declaration if possible

--- src/statements/transform_function_declaration.rs
+++ src/statements/transform_function_declaration.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-fn transform_function_name(name: &lua_ast::FunctionName) -> PatOrExpr {
+fn transform_function_name(name: &lua_ast::FunctionName) -> Expr {
 	let mut iter = name.names().iter();
 	let mut result = Expr::Ident(Ident {
 		span: Default::default(),
@@ -29,49 +29,56 @@ fn transform_function_name(name: &lua_ast::FunctionName) -> PatOrExpr {
 			}),
 		});
 	}
-	if let Expr::Ident(ident) = result {
-		PatOrExpr::Pat(boxed(Pat::Ident(BindingIdent::from(ident))))
-	} else {
-		PatOrExpr::Expr(boxed(result))
-	}
+	result
 }
 
 pub fn transform_function_declaration(declaration: &lua_ast::FunctionDeclaration) -> Stmt {
-	Stmt::Expr(ExprStmt {
+	let function = Function {
 		span: Default::default(),
-		expr: boxed(Expr::Assign(AssignExpr {
+		is_async: false,
+		is_generator: false,
+		return_type: declaration
+			.body()
+			.return_type()
+			.map(transform_type_specifier),
+		type_params: transform_type_generic(declaration.body().generics()),
+		decorators: vec![],
+		params: transform_function_params(
+			declaration.body().parameters().iter(),
+			declaration.body().type_specifiers(),
+		)
+		.into_iter()
+		.map(|param| Param {
 			span: Default::default(),
-			left: transform_function_name(declaration.name()),
-			op: AssignOp::Assign,
-			right: boxed(Expr::Fn(FnExpr {
-				ident: None,
-				function: Function {
-					span: Default::default(),
-					is_async: false,
-					is_generator: false,
-					return_type: declaration
-						.body()
-						.return_type()
-						.map(transform_type_specifier),
-					type_params: transform_type_generic(declaration.body().generics()),
-					decorators: vec![],
-					params: transform_function_params(
-						declaration.body().parameters().iter(),
-						declaration.body().type_specifiers(),
-					)
-					.into_iter()
-					.map(|param| Param {
-						span: Default::default(),
-						decorators: vec![],
-						pat: param,
-					})
-					.collect(),
-					body: Some(BlockStmt {
-						span: Default::default(),
-						stmts: transform_block_statements(declaration.body().block()),
-					}),
-				},
+			decorators: vec![],
+			pat: param,
+		})
+		.collect(),
+		body: Some(BlockStmt {
+			span: Default::default(),
+			stmts: transform_block_statements(declaration.body().block()),
+		}),
+	};
+
+	let name = transform_function_name(declaration.name());
+	if let Expr::Ident(ident) = name {
+		Stmt::Decl(Decl::Fn(FnDecl {
+			declare: false,
+			function,
+			ident,
+		}))
+	} else {
+		Stmt::Expr(ExprStmt {
+			span: Default::default(),
+			expr: boxed(Expr::Assign(AssignExpr {
+				span: Default::default(),
+				left: PatOrExpr::Expr(boxed(name)),
+				op: AssignOp::Assign,
+				right: boxed(Expr::Fn(FnExpr {
+					ident: None,
+					function,
+				})),
 			})),
-		})),
-	})
+		})
+	}
 }
